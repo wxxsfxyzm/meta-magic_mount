508354a14f83ee0c877717df5bf400d84d75e7e9
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-21 21:02:41 +0800

    scripts/metainstall: handle symlinks

--- template/metainstall.sh
+++ template/metainstall.sh
@@ -18,4 +18,21 @@ handle_partition() {
 # call install function, this is important!
 install_module
 
+mm_handle_partition() {
+	partition="$1"
+	
+	if [ ! -d "$MODPATH/system/$partition" ]; then
+		return
+	fi
+	
+	if [ -L "/system/$partition" ] && [ -d "/$partition" ]; then
+		ui_print "- Handle partition /$partition"
+		ln -sf "./system/$partition" "$MODPATH/$partition"
+	fi
+}
+
+mm_handle_partition system_ext
+mm_handle_partition vendor
+mm_handle_partition product
+
 ui_print "- Installation complete"
