1c0b602d21bc94d77a270916bab9db1bfd1f8b89
Tools-app <localhost.hutao@gmail.com>
2025-12-19 18:19:25 +0800

    chore: cleanup unused code
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/defs.rs
+++ src/defs.rs
@@ -5,7 +5,6 @@ pub const TMPFS_CANDIDATES: &[&str] = &["/debug_ramdisk", "/patch_hw", "/oem", "
 pub const DISABLE_FILE_NAME: &str = "disable";
 pub const REMOVE_FILE_NAME: &str = "remove";
 pub const SKIP_MOUNT_FILE_NAME: &str = "skip_mount";
-pub const REPLACE_DIR_FILE_NAME: &str = ".replace";
 pub const REPLACE_DIR_XATTR: &str = "trusted.overlay.opaque";
 
 // config
